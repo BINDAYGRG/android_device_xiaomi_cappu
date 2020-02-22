c9b6be3a64c0c7f456e8f239280a2ae0e6b9d370
BINDAYGRG <56853198+BINDAYGRG@users.noreply.github.com>
2020-02-22 11:13:35 +0545

    Update lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -1,13 +1,13 @@
-$(call inherit-product, device/gpd/xds/xds.mk)
+$(call inherit-product, device/xiaomi/cappu/cappu.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT="GPD/GPD/gpd_en:7.0/NRD90M/1522664419:user/test-keys" PRIVATE_BUILD_DESC="full_wisky8176_tb_n-user 7.0 NRD90M 1522664419 test-keys"
 
-PRODUCT_NAME := lineage_xds
-PRODUCT_DEVICE := xds
-PRODUCT_BRAND := GPD
-PRODUCT_MANUFACTURER := GPD
-PRODUCT_MODEL := xds
+PRODUCT_NAME := lineage_cappu
+PRODUCT_DEVICE := cappu
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := cappu
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
 PRODUCT_DEVICE="xds"
